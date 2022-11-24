491b3f13622f080c36610cd802faa19c90723f14
Hubert Jr <hubertjunior254@icloud.com>
2022-11-24 11:58:05 +0300

    work done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+const random = Math.floor(Math.random()*10) + 1;
+
+var num3 = 18;
+var num4 = 7;
+var mod = num3 % num4;
+
+var max = (10, 15, 20);
